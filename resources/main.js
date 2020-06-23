367e3a3d55203c43228d7a4e923b63502825d095
Michael David <michaeldavid@MacBook-Pro.local>
2020-06-23 17:38:09 -0400

    updated-homepage-grid

---
+++ resources/main.js
@@ -0,0 +1,11 @@
+//Animations
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.slider');
+const logo = document.querySelector('.logo');
+const hamburger = document.querySelector('.hamburger');
+const headline = document.querySelector('.text__container');
+
+
+const tl = new TimelineMax();
+
+tl.fromTo(hero,1, {height: "0%"}, {height: "80%"})
